26d9a3074d945dc7b9579653172ea7bc34912a25
MikaElliot <mikaramiandrarivo@gmail.com>
2025-10-16 13:03:39 +0300

    Main configuration

--- src/main.js
+++ src/main.js
@@ -1,41 +1,53 @@
 import './style.css'
-import * as THREE from "three";
+import * as THREE from 'three'
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-//:::::::::::::::: Scene ::::::::::::::::::::::
+//:::::::::::::::::: Scene ::::::::::::::::
 const scene = new THREE.Scene();
 
-//:::::::::::::::: Mesh (Geometry + Material)::::::::::::::::::::
+//:::::::::::::::::: Mesh(Geometry + Material) :::::::::::::::::
 const cubeGeometry = new THREE.BoxGeometry(1,1,1);
 const cubeMaterial = new THREE.MeshBasicMaterial({color: "#fefefe"});
-const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+const cubeMesh = new THREE.Mesh(cubeGeometry,cubeMaterial);
 
-scene.add(cubeMesh);
+//:::::::::::::::: Positionning :::::::::::::::::
+const tempVector = new THREE.Vector3(1,1,1);
+const cameraVector = new THREE.Vector3(1,1,5);
 
-//:::::::::::::::: Camera ::::::::::::::::
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 30);
-camera.position.z = 5;
-scene.add(camera);
+cubeMesh.position.copy(tempVector);
+const axesHelper = new THREE.AxesHelper(2);
 
-//::::::::::::::::: Renderer ::::::::::::::
-const canvas = document.querySelector("#three");
-const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+//:::::::::::::::: Camera :::::::::::::::::::
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 30);
+camera.position.copy(cameraVector);
+
+scene.add(camera, cubeMesh, axesHelper);
+
+//::::::::::::::: Renderer = render(scene, camera) ::::::::::::::::::
+const canvas = document.querySelector('#three');
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true
+});
 
 renderer.setSize(window.innerWidth, window.innerHeight);
+const maxPixelRation = Math.min(window.devicePixelRatio, 2);
+renderer.setPixelRatio(maxPixelRation);
 
-window.addEventListener('resize', () => {
+//:::::::::::::: Responsive ::::::::::::::::::
+window.addEventListener('resize', () =>{
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-//::::::::::::::::: OrbitControls :::::::::
+//:::::::::::::: Orbit Controls :::::::::::::::
 const controls = new OrbitControls(camera, canvas);
-controls.autoRotate = true;
 controls.enableDamping = true;
+controls.autoRotate = true;
 
 const renderloop = () => {
-  controls.update(); renderer.render(scene, camera);
+  controls.update(); renderer.render(scene,camera);
   window.requestAnimationFrame(renderloop);
 }
 
